a3871592039913e0095d16bbd1ce21623b06c939
abdmun8@gmail.com <abdmun8@gmail.com>
2021-01-30 17:33:31 +0700

    initial commit

---
+++ precache-manifest.0efa646e2c25bbe3131dca63013a470b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765ab04bf1818d3bff966ae01a0d576b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67b611f3fb0e892e4ef2",
+    "url": "./static/css/0.c359cadc.chunk.css"
+  },
+  {
+    "revision": "0ef3cfc278817012c396",
+    "url": "./static/css/4.d24a4378.chunk.css"
+  },
+  {
+    "revision": "741e3f5f484268f771f9",
+    "url": "./static/css/main.1930f1da.chunk.css"
+  },
+  {
+    "revision": "67b611f3fb0e892e4ef2",
+    "url": "./static/js/0.7d992e11.chunk.js"
+  },
+  {
+    "revision": "ecef787518253a5c6da06dfb3c82bca1",
+    "url": "./static/js/0.7d992e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32582349569e0b780a99",
+    "url": "./static/js/1.9f915add.chunk.js"
+  },
+  {
+    "revision": "8e7fa176b006150306288bd092a696c0",
+    "url": "./static/js/1.9f915add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f69a35bf3a64ba2dbe",
+    "url": "./static/js/10.84aa8e37.chunk.js"
+  },
+  {
+    "revision": "0ef3cfc278817012c396",
+    "url": "./static/js/4.4d66ae05.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/4.4d66ae05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5a2825f2777da4a111",
+    "url": "./static/js/5.cc58eb0c.chunk.js"
+  },
+  {
+    "revision": "3db64f2d3ed909edb2601acc876a4c46",
+    "url": "./static/js/5.cc58eb0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79112f7c950294ab6bc4",
+    "url": "./static/js/6.2d8538c8.chunk.js"
+  },
+  {
+    "revision": "991b518c5a0c2199a89e",
+    "url": "./static/js/7.d25e7f37.chunk.js"
+  },
+  {
+    "revision": "acd55b704d48585c471d",
+    "url": "./static/js/8.359c56a6.chunk.js"
+  },
+  {
+    "revision": "0d4f35a217db2413efcc",
+    "url": "./static/js/9.1418cb86.chunk.js"
+  },
+  {
+    "revision": "741e3f5f484268f771f9",
+    "url": "./static/js/main.66524ee4.chunk.js"
+  },
+  {
+    "revision": "93c2c2121ec4cf2c2b85",
+    "url": "./static/js/runtime-main.c15e3164.js"
+  },
+  {
+    "revision": "b75a802ae9c24a24cea86cf408df46cc",
+    "url": "./static/media/bg-header.b75a802a.png"
+  },
+  {
+    "revision": "54f0f681759328020e7de0c8673a0511",
+    "url": "./static/media/logo-rasyidu-hitam.54f0f681.svg"
+  },
+  {
+    "revision": "c4804ad3a098f381d076a0b2e2fb2fca",
+    "url": "./static/media/logo-rasyidu-putih.c4804ad3.svg"
+  },
+  {
+    "revision": "bb6b80e8d4fac3fe3df1e360a42f9255",
+    "url": "./static/media/logo-rasyiidu-black.bb6b80e8.png"
+  },
+  {
+    "revision": "0dde3d66ca79946e38491010a19649d2",
+    "url": "./static/media/logo-rasyiidu-icon.0dde3d66.svg"
+  },
+  {
+    "revision": "e5724745f4b04d361540063aba420d20",
+    "url": "./static/media/logo-rasyiidu-invert.e5724745.svg"
+  },
+  {
+    "revision": "dbaaf8fdbabddcc30e81e9cd68f3a7df",
+    "url": "./static/media/logo-rasyiidu.dbaaf8fd.svg"
+  }
+]);
